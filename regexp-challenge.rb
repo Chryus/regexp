a741920f8bf4b59eef81a3059e0765ae5cd3c9bf
Chris <chris.haack@me.com>
2013-11-19 17:48:39 -0500

    create regexp file

---
+++ regexp-challenge.rb
@@ -0,0 +1,7 @@
+writing a regexp that matched the following condition took eric and the simplereach team a very very long time to solve.
+ 
+can you solve it faster?
+ 
+match all URLS  *.google.*, google.*, but NOT plus.google.* or *.plus.google.*
+ 
+#challenge #pin #regexp
